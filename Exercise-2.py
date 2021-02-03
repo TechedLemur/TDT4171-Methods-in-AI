0cdea39255af5e991a8a011da00e72a0b6dcb070
Jostein Tysse <josteiht@stud.ntnu.no>
2021-02-04 00:30:16 +0100

    Add task d

--- Exercise-2.py
+++ Exercise-2.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Transition Matrix
 T = np.array([[0.8, 0.2],
               [0.3, 0.7]])
 
-B = np.array([[0.75, 0.23],
-              [0.2, 0.8]])
 
 O_1 = O_2 = O_4 = O_6 = np.array([[0.75, 0.0],
                                   [0.0, 0.2]])
@@ -25,7 +24,27 @@ def forward(t):
     x = Evidence[t-1].dot(T.transpose()).dot(forward(t-1))
     return x / np.sum(x)
 
-# Filtering
+
+def backward(k, t):
+    if k == t+1:
+        return np.array([[1],
+                         [1]])
+    return T.transpose().dot(Evidence[k-1]).dot(backward(k+1, t))
+
+
+# Prediction
+def Prediction(t, k):
+    if k == t:
+        return forward(t)
+    x = T.transpose().dot(Prediction(t, k-1))
+    return x / np.sum(x)
+
+
+# Smoothing
+def Smoothing(k, t):
+    x = forward(k)*backward(k+1, t)
+    print(x)
+    return x / np.sum(x)
 
 
 def taskB():
@@ -38,18 +57,67 @@ def taskB():
         y.append(forward(i)[0][0])
         y2.append(forward(i)[1][0])
     ax.plot(x, y, label='P(xt | e1:t)')
-    ax.plot(x, y2, label='P(~xt | e1:t)')
+    ax.plot(x, y2, label='P(¬xt | e1:t)')
     ax.set_xlabel('t')  # Add an x-label to the axes.
     ax.set_ylabel('Probability')  # Add a y-label to the axes.
-    ax.set_title("Simple Plot")  # Add a title to the axes.
+    ax.set_title("Task B, Filtering")  # Add a title to the axes.
     ax.legend()
     plt.show()
-
-# Prediction
 
 
 def taskC():
     fig, ax = plt.subplots()
+    x = []
+    y = []
+    y2 = []
+    for k in range(7, 31):
+        x.append(k)
+        y.append(Prediction(6, k)[0][0])
+        y2.append(Prediction(6, k)[1][0])
+
+    ax.plot(x, y, label='P(xt | e1:6)')
+    ax.plot(x, y2, label='P(¬xt | e1:6)')
+
+    ax.set_xlabel('t')  # Add an x-label to the axes.
+    ax.set_ylabel('Probability')  # Add a y-label to the axes.
+    ax.set_title("Task C, Prediction")  # Add a title to the axes.
+    ax.legend()
+    plt.show()
+
+
+def taskD():
+    fig, ax = plt.subplots()
+    x = []
+    y = []
+    y2 = []
+    for k in range(6):
+        s = Smoothing(k, 6)
+        print(s)
+        x.append(k)
+        y.append(s[0][0])
+        y2.append(s[1][0])
+
+    ax.plot(x, y, '-o', label='P(xt | e1:6)')
+    ax.plot(x, y2, '-o', label='P(¬xt | e1:6)')
+    for a, b in zip(x, y):
+        ax.annotate(s="{:.4f}".format(b),
+                    xy=(a, b),
+                    textcoords='offset points',
+                    xytext=(0, 10),
+                    ha=('center'))
+    for a, b in zip(x, y2):
+        ax.annotate(s="{:.4f}".format(b),
+                    xy=(a, b),
+                    textcoords='offset points',
+                    xytext=(0, 10),
+                    ha=('center'))
+    ax.set_xlabel('t')  # Add an x-label to the axes.
+    ax.set_ylabel('Probability')  # Add a y-label to the axes.
+    ax.set_title("Task D, Smoothing")  # Add a title to the axes.
+    ax.legend()
+    plt.show()
 
 
 taskB()
+taskC()
+taskD()
