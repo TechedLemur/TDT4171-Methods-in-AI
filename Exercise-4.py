bb952f18a76524a3945a67c75b18363924b6a650
Jostein Tysse <josteiht@stud.ntnu.no>
2021-03-12 22:55:58 +0100

    Gain works

--- Exercise-4.py
+++ Exercise-4.py
@@ -5,13 +5,21 @@ import math
 
 
 def B(q):
+    if (q in [0, 1]):
+        return 0
     return -(q*math.log(q, 2)+(1-q)*math.log(1-q, 2))
 
 
 def Remainder(A):
     r = 0
-    for k in data.groupby("Survived")[A]:
-        print(k)
+    grouped = data.groupby("WillWait")[
+        A].value_counts().unstack(fill_value=0).stack()
+
+    for k in data[A].unique():
+        p_k = grouped[(1, k)]
+        n_k = grouped[(0, k)]
+        r += (p_k + n_k)/(p + n) * B(p_k / (p_k + n_k))
+    return r
 
 
 def Gain(A):
@@ -25,10 +33,10 @@ class Node:
 
 
 columns = []
-columns.append("Survived")
+# columns.append("Survived")
 # columns.append("Pclass")
-columns.append("Name")
-columns.append("Sex")  # Relevant
+# columns.append("Name")
+# columns.append("Sex")  # Relevant
 # columns.append("Age") # Continuous - Relevant
 # columns.append("SibSp") # Continuous - Relevant?
 # columns.append("Parch") # Contniuous - Relevant?
@@ -38,14 +46,39 @@ columns.append("Sex")  # Relevant
 # columns.append("Embarked")
 
 
-data = pd.read_csv("./train.csv", usecols=columns)
+# data = pd.read_csv("./train.csv", usecols=columns)
+data = pd.read_csv("./book.csv")
 
-p = data["Survived"].value_counts()[1]
-n = data["Survived"].value_counts()[0]
+# p = data["Survived"].value_counts()[1]
+p = data["WillWait"].value_counts()[1]
+# n = data["Survived"].value_counts()[0]
+n = data["WillWait"].value_counts()[0]
 
 g = B(p/(p+n))
 
-# print(df.groupby("SibSp")["Name"].value_counts())
+# Remainder("Patrons")
+count = data.groupby("WillWait")[
+    "Patrons"].value_counts().unstack(fill_value=0).stack()
+# for c in count.items():
+#   print(c)
+a = "Patrons"
+vals = data.Patrons.unique()
 
+
+def test(b):
+
+    print(data.groupby("WillWait")
+          [b].value_counts().unstack(fill_value=0).stack())
+
+
+# for v in vals:
+# test(a)
+#    print(v)
+#    print(count[(0, v)], "false")
+#    print(count[(1, v)], "true")
+# print(count[[0][0]])
+# print(data)
 # Remainder("Sex")
-print(g)
+# print(g)
+
+print(Gain("Patrons"))
