da159b1dc860d931dc5fca83ae80c2f5d2a8bce6
Jostein <josteiht@stud.ntnu.no>
2021-04-25 23:40:25 +0200

    Remove old implementation and add new inits

--- Exercise-5.py
+++ Exercise-5.py
@@ -50,15 +50,21 @@ class NeuralNetwork:
         self.input_dim = input_dim
         self.hidden_layer = hidden_layer
 
-        self.input_layer = Layer(input_dim)
+        rng = np.random.default_rng(12345)
 
-        self.output_layer = Layer(OUTPUT_SIZE)
         if (hidden_layer):
-            h_L = Layer(self.hidden_units)
-            self.input_layer.connect(h_L)
-            h_L.connect(self.output_layer)
+            self.W = [rng.random(25, 25), rng.random(25)]
+            self.D = [np.zeros(25), np.zeros(25)
+            self.B = [rng.random(25), rng.random(1)]
+            self.A = [np.zeros(25), np.zeros(25), np.zeros(1)]
+            self.In = [np.zeros(25), np.zeros(25), np.zeros(1)]
         else:
-            self.input_layer.connect(self.output_layer)
+            self.W = rng.random(25)
+            self.D = np.zeros(25)
+            self.B = rng.random(25)
+            self.A = [np.zeros(25), np.zeros(1)]
+            self.In = [np.zeros(25), np.zeros(1)]
+        
 
     def load_data(self, file_path: str = os.path.join(os.getcwd(), 'data_breast_cancer.p')) -> None:
         """
@@ -92,65 +98,12 @@ class NeuralNetwork:
         # Line 6 in Figure 18.24 says "repeat".
         # We are going to repeat self.epochs times as written in the __init()__ method.
 
-        print(len(self.x_train))
-        print(len(self.y_train))
-        print(len(self.x_train[0]))
         for _ in range(self.epochs):
-            tic = time.perf_counter()
-            print(1)
             for x, y in zip(self.x_train, self.y_train):
-                print(2)
-                print(tic - time.perf_counter())
-                for node, value in zip(self.input_layer.nodes, x):
-                    node.value = value
-                    node.in_v = value
-
-                current_layer = self.input_layer.next_layer
-                while (current_layer):
-
-                    for j in current_layer.nodes:
-                        in_j = 0.0
-                        for i in j.incoming_nodes:
-                            in_j += i[0].value * i[1]
-                        j.in_v = in_j
-                        j.value = self.g(in_j)
-                    current_layer = current_layer.next_layer
-                print(3)
-                print(tic - time.perf_counter())
-                for j in self.output_layer.nodes:  # Can support multiple output nodes, but in this case we only have one
-                    j.delta = self.g_prime(j.in_v) * (y - j.value)
-
-                current_layer = self.output_layer.previous_layer
-                print(4)
-                print(tic - time.perf_counter())
-                while(current_layer):
-                    next_layer = current_layer.next_layer
-
-                    for i in range(len(current_layer.nodes)):
-                        d_i = 0.0
-                        node_i = current_layer.nodes[i]
-
-                        for j in next_layer.nodes:
-                            d_i += self.g_prime(node_i.in_v) * \
-                                j.incoming_nodes[i][1] * j.delta
-
-                        node_i.delta = d_i
-
-                    current_layer = current_layer.previous_layer
-                print(5)
-                print(tic - time.perf_counter())
-                current_layer = self.input_layer.next_layer
-                while(current_layer):
-
-                    for node in current_layer.nodes:
-
-                        for in_node in node.incoming_nodes:
-                            n = in_node[0]
-                            w = in_node[1]
-                            in_node[1] = w + self.lr * n.value * node.delta
-                    current_layer = current_layer.next_layer
-                print(6)
-                print(tic - time.perf_counter())
+                pass
+        
+        
+        
         # Line 27 in Figure 18.24 says "return network". Here you do not need to return anything as we are coding
         # the neural network as a class
 
@@ -162,18 +115,7 @@ class NeuralNetwork:
         :return: A float specifying probability which is bounded [0, 1].
         """
         # TODO: Implement the forward pass.
-
-        current_layer = self.input_layer.next_layer
-        while (current_layer):
-
-            for j in current_layer.nodes:
-                in_j = 0.0
-                for i in j.incoming_nodes:
-                    in_j += i[0].value * i[1]
-                j.in_v = in_j
-                j.value = self.g(in_j)
-            current_layer = current_layer.next_layer
-        return self.output_layer.nodes[0].value
+        pass
 
 
 class TestAssignment5(unittest.TestCase):
@@ -226,37 +168,6 @@ class TestAssignment5(unittest.TestCase):
                         'This implementation is most likely wrong since '
                         f'the accuracy ({accuracy}) is less than {self.threshold}.')
 
-
-class Neuron:
-    # TODO: Decide on class structure
-    """
-    Class for representing a neuron. The "inputs" array keeps track if incoming links,
-    and the "weights" array has the input weights on the corresponding index.
-    """
-
-    def __init__(self, incoming_nodes=[]) -> None:
-
-        self.incoming_nodes = incoming_nodes
-        self.value = 0.0
-
-    def __repr__(self):
-        return f'Neuron: {self.value}, {self.in_v}'
-
-
-class Layer:
-
-    def __init__(self, size: int):
-        self.next_layer = None
-        self.previous_layer = None
-        self.nodes = [Neuron() for _ in range(size)]
-
-    def connect(self, layer):
-        self.next_layer = layer
-        layer.previous_layer = self
-        for node in self.nodes:
-            for n in layer.nodes:
-                # Add links and init weights to a small random number
-                n.incoming_nodes.append([node, random.uniform(-0.1, 0.1)])
 
 
 if __name__ == '__main__':
